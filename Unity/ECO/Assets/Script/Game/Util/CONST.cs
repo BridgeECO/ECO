7e489cdbdfa413db6616655d0590223c65e898c0
imhyunjun <limhyunjun04@naver.com>
2025-09-21 16:30:35 +0900

    [임현준] 모듈 버튼

---
+++ Unity/ECO/Assets/Script/Game/Util/CONST.cs
@@ -0,0 +1,8 @@
+﻿namespace ECO
+{
+    public static class CONST
+    {
+        public const int DEF_IDX = 0;
+        public const int INVALID_IDX = -1;
+    }
+}
---
+++ Unity/ECO/Assets/Script/Game/Util/TEXT.cs
@@ -0,0 +1,10 @@
+﻿public class TEXT
+{
+    public const string PLAY_SCENE_NAME = "PlayScene";
+    public const string TITLE_SCENE_NAME = "TitleScene";
+
+    //실제 텍스트는 코드에 들어가지 않음
+
+    //유니티 태그들
+    public const string PLAYER_TAG = "Player";
+}
