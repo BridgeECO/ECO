c18f595fe065341982b000026a4368a63416d8cb
imhyunjun <limhyunjun04@naver.com>
2025-10-26 14:21:19 +0900

    [임현준] 타일맵 추가

---
+++ Unity/ECO/Assets/Script/Game/Map/Platform.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace ECO
+{
+    public class Platform : MonoBase
+    {
+        [SerializeField] private AkEvent _wwiseEvt = null;
+
+
+        protected override void OnDestroyMono()
+        {
+
+        }
+
+        protected override bool OnCreateMono()
+        {
+            return true;
+        }
+
+        public void PlayWwiseEvt()
+        {
+            if (_wwiseEvt == null)
+                return;
+
+            AkSoundEngine.PostEvent(_wwiseEvt.playingId, this.gameObject);
+        }
+    }
+}
