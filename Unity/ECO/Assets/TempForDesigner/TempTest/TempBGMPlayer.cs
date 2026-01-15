eebef46e65606d69e61ed748c7ffe19b43e4de7b
helloSalmon <94889770+helloSalmon@users.noreply.github.com>
2026-01-15 09:44:02 +0900

    [송준] 더미 코드 - 배경음악 재생 기능 추가
    여러 개의 배경음악을 동시에 재생할 수 있으면서 언제든 껏다 켰다 할 수 있도록 함

--- Unity/ECO/Assets/TempForDesigner/TempTest/TempBGMPlayer.cs
+++ Unity/ECO/Assets/TempForDesigner/TempTest/TempBGMPlayer.cs
@@ -6,22 +6,38 @@ using UnityEngine;
 public class TempBGMPlayer : MonoBehaviour
 {
     [SerializeField]
-    private List<AkEvent> bgmSounds;
+    private List<AudioSource> bgmSounds;
 
     public List<bool> isBgmOns;
-
-    [SerializeField]
-    private AkEvent originalBgmSource;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //배경음악이 언제든 켜져도 전체 음악에 어색하지 않고 바로 들어갈 수 있도록, 
+    // 오디오를 껐다 키는 대신 음소거여부를 조절하는 방식으로 대응
     void Start()
     {
-        bgmSounds = GetComponentsInChildren<AkEvent>().ToList();
+        //배경음악 오브젝트들 전부 등록해서 일단은 음소거시킴
+        bgmSounds = GetComponentsInChildren<AudioSource>().ToList();
+        foreach (AudioSource audioSource in bgmSounds)
+        {
+            audioSource.mute = true;
+            audioSource.Play();
+
+            //그리고 각 오디오 소스 당 하나씩 isBgmOns에 bool을 등록
+            isBgmOns.Add(false);
+        }
+
+
     }
 
-    // Update is called once per frame
+    // 여기서는, 만약 isBgmOns가 True 값이라면, 거기에 대응되는 AudioSource의 볼륨을 원상복귀시킴
     void Update()
     {
-        
+        for(int i=0; i < isBgmOns.Count; i++)
+        {
+            // mute 상태 업데이트
+            if (bgmSounds[i].mute != !isBgmOns[i])
+            {
+                bgmSounds[i].mute = !isBgmOns[i];
+            }
+        }
     }
 }
