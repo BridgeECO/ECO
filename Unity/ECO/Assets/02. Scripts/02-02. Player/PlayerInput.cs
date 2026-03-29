d22bd00074d6693187130aef985a131323d204b1
Mingyu Choi <mg010626@naver.com>
2026-03-29 14:56:52 +0900

    Feat | PlayerInput 구현

--- Unity/ECO/Assets/02. Scripts/02-02. Player/PlayerInput.cs
+++ Unity/ECO/Assets/02. Scripts/02-02. Player/PlayerInput.cs
@@ -1,6 +1,52 @@
 using UnityEngine;
+using System;
 
 public class PlayerInput : MonoBehaviour
 {
-   
+    public Action OnJumpPressed;
+    public Action OnJumpReleased;
+    public Action OnDashPressed;
+    public Action OnDashReleased;
+
+    public float HorizontalInput { get; private set; }
+    public Vector2 MouseWorldPosition { get; private set; }
+
+    private Camera _mainCamera;
+
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        HorizontalInput = Input.GetAxisRaw("Horizontal");
+        MouseWorldPosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        Jump();
+        Dash();
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnJumpPressed?.Invoke();
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            OnJumpReleased?.Invoke();
+        }
+    }
+
+    private void Dash()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnDashPressed?.Invoke();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            OnDashReleased?.Invoke();
+        }
+    }
 }
