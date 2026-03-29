fb1469c715ab6b00f1e8c40cfc1825a820c3c4db
Mingyu Choi <mg010626@naver.com>
2026-03-29 14:57:03 +0900

    Feat | PlayerMotor 구현

--- Unity/ECO/Assets/02. Scripts/02-02. Player/PlayerMotor.cs
+++ Unity/ECO/Assets/02. Scripts/02-02. Player/PlayerMotor.cs
@@ -2,5 +2,42 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
-    
+    public Vector2 Velocity { get; private set; }
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.linearVelocity = Velocity;
+    }
+
+    public void SetVelocity(Vector2 newVelocity)
+    {
+        Velocity = newVelocity;
+    }
+
+    public void SetVelocityX(float x)
+    {
+        Velocity = new Vector2(x, Velocity.y);
+    }
+
+    public void SetVelocityY(float y)
+    {
+        Velocity = new Vector2(Velocity.x, y);
+    }
+
+    public void AddVelocity(Vector2 addedVelocity)
+    {
+        Velocity += addedVelocity;
+    }
+
+    public void Teleport(Vector2 position)
+    {
+        _rigidbody.position = position;
+    }
 }
