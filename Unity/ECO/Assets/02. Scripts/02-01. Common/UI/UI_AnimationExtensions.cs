afa6cfd3f243e8e1b5ba80f013bca7289d00575c
Mingyu Choi <mg010626@naver.com>
2026-04-01 13:44:30 +0900

    Feat | UI 애니메이션을 담당하는 확장 클래스 구현

---
+++ Unity/ECO/Assets/02. Scripts/02-01. Common/UI/UI_AnimationExtensions.cs
@@ -0,0 +1,111 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class UI_AnimationExtensions
+{
+    public static void Slide(this RectTransform target, float slideTime, float scalarSlideAnimation, params System.Action[] onCompleteActions)
+    {
+        target.DOAnchorPosX(target.anchoredPosition.x + scalarSlideAnimation, slideTime)
+            .OnComplete(() =>
+            {
+                foreach (var action in onCompleteActions)
+                {
+                    action?.Invoke();
+                }
+            });
+    }
+
+    public static void SlideIn(this RectTransform left, RectTransform right, float slideTime, float scalarSlideAnimation, params System.Action[] onCompleteActions)
+    {
+        left.DOAnchorPosX(left.anchoredPosition.x + scalarSlideAnimation, slideTime);
+        right.DOAnchorPosX(right.anchoredPosition.x - scalarSlideAnimation, slideTime)
+            .OnComplete(() =>
+            {
+                foreach (var action in onCompleteActions)
+                {
+                    action?.Invoke();
+                }
+            });
+    }
+
+    public static void SlideOut(this RectTransform left, RectTransform right, float slideTime, float scalarSlideAnimation, params System.Action[] onCompleteActions)
+    {
+        left.DOAnchorPosX(left.anchoredPosition.x - scalarSlideAnimation, slideTime).SetEase(Ease.OutQuad);
+        right.DOAnchorPosX(right.anchoredPosition.x + scalarSlideAnimation, slideTime).SetEase(Ease.OutQuad)
+            .OnComplete(() =>
+            {
+                foreach (var action in onCompleteActions)
+                {
+                    action?.Invoke();
+                }
+            });
+    }
+
+    public static void Fade(this Graphic graphic, float targetFadeValue, float fadeTime, params System.Action[] onCompleteActions)
+    {
+        graphic.DOFade(targetFadeValue, fadeTime).SetEase(Ease.OutQuad).OnComplete(() =>
+        {
+            foreach (var action in onCompleteActions)
+            {
+                action?.Invoke();
+            }
+        });
+    }
+
+    public static void Fade(this CanvasGroup canvasGroup, float targetFadeValue, float fadeTime, params System.Action[] onCompleteActions)
+    {
+        canvasGroup.DOFade(targetFadeValue, fadeTime).SetEase(Ease.OutQuad).OnComplete(() =>
+        {
+            foreach (var action in onCompleteActions)
+            {
+                action?.Invoke();
+            }
+        });
+    }
+
+    public static void PopIn(this RectTransform target, float time, params System.Action[] onCompleteActions)
+    {
+        target.localScale = Vector3.zero;
+        target.DOScale(Vector3.one, time).SetEase(Ease.OutBack).OnComplete(() =>
+        {
+            foreach (var action in onCompleteActions)
+            {
+                action?.Invoke();
+            }
+        });
+    }
+
+    public static void PopOut(this RectTransform target, float time, params System.Action[] onCompleteActions)
+    {
+        target.DOScale(Vector3.zero, time).SetEase(Ease.InBack).OnComplete(() =>
+        {
+            foreach (var action in onCompleteActions)
+            {
+                action?.Invoke();
+            }
+        });
+    }
+
+    public static void PunchScale(this RectTransform target, float duration = 0.2f, params System.Action[] onCompleteActions)
+    {
+        target.DOPunchScale(new Vector3(0.1f, 0.1f, 0.1f), duration, 1, 1f).OnComplete(() =>
+        {
+            foreach (var action in onCompleteActions)
+            {
+                action?.Invoke();
+            }
+        });
+    }
+
+    public static void Shake(this RectTransform target, float duration = 0.5f, params System.Action[] onCompleteActions)
+    {
+        target.DOShakeAnchorPos(duration, new Vector2(10f, 0f), 10, 90f, false, true).OnComplete(() =>
+        {
+            foreach (var action in onCompleteActions)
+            {
+                action?.Invoke();
+            }
+        });
+    }
+}
