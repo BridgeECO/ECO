2bb0dc27f17d12c9b41e03053769fa6d4d705f19
Mingyu Choi <mg010626@naver.com>
2026-04-02 11:08:45 +0900

    Docs | 에디터 스크립트 폴더 위치 스크립트 폴더 내부로 변경

---
+++ Unity/ECO/Assets/02. Scripts/Editor/SceneGridOverlay.cs
@@ -0,0 +1,113 @@
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class SceneGridSystem
+{
+    private static bool _showGrid;
+    private static float _gridSize;
+    private static Color _gridColor = Color.black;
+
+    static SceneGridSystem()
+    {
+        _showGrid = EditorPrefs.GetBool("ECO_ShowGrid", true);
+        _gridSize = EditorPrefs.GetFloat("ECO_GridSize", 0.25f);
+
+        string colorStr = EditorPrefs.GetString("ECO_GridColor", "#FFFFFF33");
+        if (!ColorUtility.TryParseHtmlString(colorStr, out _gridColor))
+        {
+            _gridColor = Color.black;
+        }
+
+        SceneView.duringSceneGui -= OnSceneGUI;
+        SceneView.duringSceneGui += OnSceneGUI;
+    }
+
+    [MenuItem("ECO/Grid/Toggle Scene Grid", priority = 1)]
+    public static void ToggleGrid()
+    {
+        _showGrid = !_showGrid;
+        EditorPrefs.SetBool("ECO_ShowGrid", _showGrid);
+        SceneView.RepaintAll();
+    }
+
+    [MenuItem("ECO/Grid/Toggle Scene Grid", true)]
+    public static bool ToggleGridValidate()
+    {
+        Menu.SetChecked("ECO/Grid/Toggle Scene Grid", _showGrid);
+        return true;
+    }
+
+    public static float GridSize
+    {
+        get => _gridSize;
+        set
+        {
+            _gridSize = value;
+            EditorPrefs.SetFloat("ECO_GridSize", _gridSize);
+        }
+    }
+
+    public static Color GridColor
+    {
+        get => _gridColor;
+        set
+        {
+            _gridColor = value;
+            EditorPrefs.SetString("ECO_GridColor", "#" + ColorUtility.ToHtmlStringRGBA(_gridColor));
+        }
+    }
+
+    private static void OnSceneGUI(SceneView sceneView)
+    {
+        if (!_showGrid || _gridSize <= 0f) return;
+
+        Handles.color = _gridColor;
+
+        Camera cam = sceneView.camera;
+        Vector3 camPos = cam.transform.position;
+        float height = cam.orthographicSize * 2f;
+        float width = height * cam.aspect;
+
+        float startX = Mathf.Floor((camPos.x - width / 2f) / _gridSize) * _gridSize;
+        float endX = camPos.x + width / 2f;
+        float startY = Mathf.Floor((camPos.y - height / 2f) / _gridSize) * _gridSize;
+        float endY = camPos.y + height / 2f;
+
+        for (float x = startX; x <= endX; x += _gridSize)
+        {
+            Handles.DrawLine(new Vector3(x, startY, 0f), new Vector3(x, endY, 0f));
+        }
+
+        for (float y = startY; y <= endY; y += _gridSize)
+        {
+            Handles.DrawLine(new Vector3(startX, y, 0f), new Vector3(endX, y, 0f));
+        }
+    }
+}
+
+public class SceneGridSettingsWindow : EditorWindow
+{
+    [MenuItem("ECO/Grid/Grid Settings", priority = 2)]
+    public static void ShowWindow()
+    {
+        GetWindow<SceneGridSettingsWindow>("Grid Settings");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Grid Settings", EditorStyles.boldLabel);
+
+        EditorGUI.BeginChangeCheck();
+
+        float newSize = EditorGUILayout.FloatField("Grid Size (Unit)", SceneGridSystem.GridSize);
+        Color newColor = EditorGUILayout.ColorField("Grid Color", SceneGridSystem.GridColor);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            SceneGridSystem.GridSize = newSize;
+            SceneGridSystem.GridColor = newColor;
+            SceneView.RepaintAll();
+        }
+    }
+}
