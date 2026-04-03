30f04bee38908ef61341710fccaac4eadeda18b5
Mingyu0626 <mg010626@naver.com>
2026-04-03 12:47:09 +0900

    Fix | GameSceneManager 제네릭 싱글톤 상속하도록 수정

--- Unity/ECO/Assets/02. Scripts/02-20. SceneLogic/GameSceneManager.cs
+++ Unity/ECO/Assets/02. Scripts/02-20. SceneLogic/GameSceneManager.cs
@@ -2,28 +2,18 @@ using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameSceneManager : MonoBehaviour
+public class GameSceneManager : MonoBehaviourSingleton<GameSceneManager>
 {
-    public static GameSceneManager Instance;
-
     private string _currentLoadedRegionScene;
 
-    private void Awake()
+    protected override void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+        base.Awake();
     }
 
     private void Start()
     {
-        LoadRegionSceneAsync("Region1Scene").Forget();
+        LoadRegionSceneAsync(nameof(ESceneNames.Region1Scene)).Forget();
     }
 
     private async UniTask LoadRegionSceneAsync(string sceneName)
@@ -55,6 +45,5 @@ public class GameSceneManager : MonoBehaviour
     public async UniTask TransitionToNewRegionAsync(string targetSceneName)
     {
         await LoadRegionSceneAsync(targetSceneName);
-
     }
 }
