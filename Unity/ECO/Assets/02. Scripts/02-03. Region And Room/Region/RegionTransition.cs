8679dd820e8436dbc432480955bc45b0360fb587
Mingyu Choi <mg010626@naver.com>
2026-04-06 17:14:39 +0900

    Remove | 주석 제거

--- Unity/ECO/Assets/02. Scripts/02-03. Region And Room/Region/RegionTransition.cs
+++ Unity/ECO/Assets/02. Scripts/02-03. Region And Room/Region/RegionTransition.cs
@@ -1,11 +1,6 @@
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
-// 1. 화면 페이드 아웃
-// 2. 기존 씬 언로드
-// 3. 새 씬 로드
-// 4. 플레이어 좌표 조정
-// 5. 화면 페이드 인
 [RequireComponent(typeof(BoxCollider2D))]
 public class RegionTransition : MonoBehaviour
 {
