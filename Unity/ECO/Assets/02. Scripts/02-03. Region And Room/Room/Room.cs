973f115376aa9b05a3dcf07e65c3fbc9fd79254f
Mingyu Choi <mg010626@naver.com>
2026-04-06 16:47:37 +0900

    Refactor | PPU에 따른 Room의 가로 세로 길이 Refresh 로직 간략화
    기존 에디터 코드 폐기

--- Unity/ECO/Assets/02. Scripts/02-03. Region And Room/Room/Room.cs
+++ Unity/ECO/Assets/02. Scripts/02-03. Region And Room/Room/Room.cs
@@ -4,6 +4,8 @@ using VInspector;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Room : MonoBehaviour
 {
+    private const int PPU = 256;
+
     [Foldout("Hierarchy")]
     [SerializeField]
     private BoxCollider2D _cameraBounds;
@@ -11,7 +13,25 @@ public class Room : MonoBehaviour
     [SerializeField]
     private Transform _spawnPoint;
 
+    [Foldout("Pixel Boundary")]
+    [SerializeField]
+    private float _pixelWidth;
+
+    [SerializeField]
+    private float _pixelHeight;
+
     public Vector2 MinBounds => _cameraBounds.bounds.min;
     public Vector2 MaxBounds => _cameraBounds.bounds.max;
     public Vector3 SpawnPoint => _spawnPoint.position;
+
+    [Button]
+    private void RefreshBoundary()
+    {
+        if (_cameraBounds != null)
+        {
+            _cameraBounds.isTrigger = true;
+            _cameraBounds.size = new Vector2(_pixelWidth / PPU, _pixelHeight / PPU);
+            _cameraBounds.compositeOperation = Collider2D.CompositeOperation.Merge;
+        }
+    }
 }
