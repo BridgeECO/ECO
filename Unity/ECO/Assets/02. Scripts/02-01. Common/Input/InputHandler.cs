bff7a9c3b55441e07127282f7614c63ac7aa8285
Mingyu Choi <mg010626@naver.com>
2026-04-16 15:39:31 +0900

    Feat | InputHandler에서 UI 인풋과 플레이어 인풋을 구분하여 제어

--- Unity/ECO/Assets/02. Scripts/02-01. Common/Input/InputHandler.cs
+++ Unity/ECO/Assets/02. Scripts/02-01. Common/Input/InputHandler.cs
@@ -1,6 +1,19 @@
+using System;
+
 public static class InputHandler
 {
+    public enum EInputMode
+    {
+        Player,
+        UI
+    }
+
     public static bool IsInputBlocked { get; private set; }
+    public static EInputMode CurrentMode { get; private set; } = EInputMode.Player;
+
+    public static Action OnCancelEvent;
+    public static Action OnNavigateEvent;
+    public static Action OnPointEvent;
 
     public static void BlockInput()
     {
@@ -11,4 +24,20 @@ public static class InputHandler
     {
         IsInputBlocked = false;
     }
+
+    public static void ChangeToUIInput()
+    {
+        CurrentMode = EInputMode.UI;
+        BlockInput(); // 인게임 로직 차단
+    }
+
+    public static void ChangeToPlayerInput()
+    {
+        CurrentMode = EInputMode.Player;
+        UnblockInput(); // 인게임 로직 허용
+    }
+
+    public static void TriggerCancelEvent() => OnCancelEvent?.Invoke();
+    public static void TriggerNavigateEvent() => OnNavigateEvent?.Invoke();
+    public static void TriggerPointEvent() => OnPointEvent?.Invoke();
 }
