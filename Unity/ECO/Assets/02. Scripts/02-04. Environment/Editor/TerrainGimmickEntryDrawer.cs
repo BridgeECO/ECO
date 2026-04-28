9d9d1cde5e954a4ad2328fb96c2648dec86cfbd8
Mingyu Choi <mg010626@naver.com>
2026-04-28 17:44:05 +0900

    Feat | TerrainGimmickEntry에서 추가된 기믹에 필요한 데이터만 인스펙터에 노출시키는 기능 구현

--- Unity/ECO/Assets/02. Scripts/02-04. Environment/Editor/TerrainGimmickEntryDrawer.cs
+++ Unity/ECO/Assets/02. Scripts/02-04. Environment/Editor/TerrainGimmickEntryDrawer.cs
@@ -10,17 +10,33 @@ public class TerrainGimmickEntryDrawer : PropertyDrawer
 
         SerializedProperty gimmickDataProperty = property.FindPropertyRelative("_gimmickData");
         SerializedProperty changeSpriteProperty = property.FindPropertyRelative("_changeSprite");
+        SerializedProperty moveSpeedProperty = property.FindPropertyRelative("_moveSpeed");
+        SerializedProperty waypointsProperty = property.FindPropertyRelative("_waypoints");
 
         float lineHeight = EditorGUIUtility.singleLineHeight;
         float spacing = EditorGUIUtility.standardVerticalSpacing;
 
-        Rect gimmickDataRect = new Rect(position.x, position.y, position.width, lineHeight);
-        EditorGUI.PropertyField(gimmickDataRect, gimmickDataProperty, label);
+        Rect currentRect = new Rect(position.x, position.y, position.width, lineHeight);
+        EditorGUI.PropertyField(currentRect, gimmickDataProperty, label);
 
         if (IsImageChangeGimmick(gimmickDataProperty))
         {
-            Rect changeSpriteRect = new Rect(position.x, position.y + lineHeight + spacing, position.width, lineHeight);
-            EditorGUI.PropertyField(changeSpriteRect, changeSpriteProperty, new GUIContent("Change Sprite"));
+            currentRect.y += lineHeight + spacing;
+            EditorGUI.PropertyField(currentRect, changeSpriteProperty, new GUIContent("Change Sprite"));
+        }
+
+        if (IsMoveTerrainGimmick(gimmickDataProperty) || IsPatrolTerrainGimmick(gimmickDataProperty))
+        {
+            currentRect.y += lineHeight + spacing;
+            EditorGUI.PropertyField(currentRect, moveSpeedProperty, new GUIContent("Move Speed"));
+        }
+
+        if (IsMoveTerrainGimmick(gimmickDataProperty) || IsPatrolTerrainGimmick(gimmickDataProperty))
+        {
+            currentRect.y += lineHeight + spacing;
+            float waypointsHeight = EditorGUI.GetPropertyHeight(waypointsProperty, true);
+            currentRect.height = waypointsHeight;
+            EditorGUI.PropertyField(currentRect, waypointsProperty, new GUIContent("Waypoints"), true);
         }
 
         EditorGUI.EndProperty();
@@ -29,6 +45,7 @@ public class TerrainGimmickEntryDrawer : PropertyDrawer
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
         SerializedProperty gimmickDataProperty = property.FindPropertyRelative("_gimmickData");
+        SerializedProperty waypointsProperty = property.FindPropertyRelative("_waypoints");
 
         float lineHeight = EditorGUIUtility.singleLineHeight;
         float spacing = EditorGUIUtility.standardVerticalSpacing;
@@ -38,6 +55,16 @@ public class TerrainGimmickEntryDrawer : PropertyDrawer
         if (IsImageChangeGimmick(gimmickDataProperty))
         {
             height += lineHeight + spacing;
+        }
+
+        if (IsMoveTerrainGimmick(gimmickDataProperty) || IsPatrolTerrainGimmick(gimmickDataProperty))
+        {
+            height += lineHeight + spacing;
+        }
+
+        if (IsMoveTerrainGimmick(gimmickDataProperty) || IsPatrolTerrainGimmick(gimmickDataProperty))
+        {
+            height += EditorGUI.GetPropertyHeight(waypointsProperty, true) + spacing;
         }
 
         return height;
@@ -51,5 +78,25 @@ public class TerrainGimmickEntryDrawer : PropertyDrawer
         }
 
         return gimmickDataProperty.objectReferenceValue is ImageChangeGimmickSO;
+    }
+
+    private bool IsMoveTerrainGimmick(SerializedProperty gimmickDataProperty)
+    {
+        if (gimmickDataProperty.objectReferenceValue == null)
+        {
+            return false;
+        }
+
+        return gimmickDataProperty.objectReferenceValue is MoveTerrainGimmickSO;
+    }
+
+    private bool IsPatrolTerrainGimmick(SerializedProperty gimmickDataProperty)
+    {
+        if (gimmickDataProperty.objectReferenceValue == null)
+        {
+            return false;
+        }
+
+        return gimmickDataProperty.objectReferenceValue is PatrolTerrainGimmickSO;
     }
 }
