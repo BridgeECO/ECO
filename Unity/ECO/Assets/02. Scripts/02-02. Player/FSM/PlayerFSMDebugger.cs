accf15224383d04ddd073300c37f142d0be365f8
Mingyu Choi <mg010626@naver.com>
2026-05-07 10:59:50 +0900

    Feat | 플레이어 FSM의 상태 트랜지션 기록 및 쿨다운 조회를 위한 디버깅 툴 구현

---
+++ Unity/ECO/Assets/02. Scripts/02-02. Player/FSM/PlayerFSMDebugger.cs
@@ -0,0 +1,94 @@
+using System.Collections.Generic;
+using UnityEngine;
+using VInspector;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class PlayerFSMDebugger : MonoBehaviour
+{
+    [Foldout("Hierarchy")]
+    [SerializeField]
+    private PlayerStateMachine _stateMachine;
+
+    [Foldout("Debug")]
+    [SerializeField, ReadOnly]
+    private string _currentState;
+
+    [SerializeField, ReadOnly]
+    private string _transitionHistory;
+
+    [Foldout("Timers")]
+    [SerializeField, ReadOnly]
+    private float _jumpBufferTimer;
+
+    [SerializeField, ReadOnly]
+    private float _coyoteTimer;
+
+    [SerializeField, ReadOnly]
+    private float _dashCooldownTimer;
+
+    [SerializeField, ReadOnly]
+    private float _inputLockTimer;
+
+    private Queue<EPlayerState> _historyQueue = new Queue<EPlayerState>();
+
+    private void OnEnable()
+    {
+        if (_stateMachine == null)
+        {
+            return;
+        }
+        _stateMachine.OnStateChanged += HandleStateChanged;
+    }
+
+    private void OnDisable()
+    {
+        if (_stateMachine == null)
+        {
+            return;
+        }
+        _stateMachine.OnStateChanged -= HandleStateChanged;
+    }
+
+    private void Update()
+    {
+        if (ReferenceEquals(_stateMachine, null))
+        {
+            return;
+        }
+
+        _jumpBufferTimer = _stateMachine.JumpBufferTimer;
+        _coyoteTimer = _stateMachine.CoyoteTimer;
+        _dashCooldownTimer = _stateMachine.DashCooldownTimer;
+        _inputLockTimer = _stateMachine.InputLockTimer;
+    }
+
+    private void HandleStateChanged(EPlayerState newState)
+    {
+        _currentState = newState.ToString();
+        _historyQueue.Enqueue(newState);
+
+        if (4 < _historyQueue.Count)
+        {
+            _historyQueue.Dequeue();
+        }
+
+        _transitionHistory = string.Join(" -> ", _historyQueue);
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (_stateMachine == null)
+        {
+            return;
+        }
+
+        if (Application.isPlaying)
+        {
+            Handles.Label(transform.position + Vector3.up * 1.5f, _currentState);
+        }
+    }
+#endif
+}
