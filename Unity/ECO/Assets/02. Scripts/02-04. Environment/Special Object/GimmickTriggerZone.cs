f819e0cc4a0abfd9b4c69338d7dbb9f99add0a3b
Mingyu Choi <mg010626@naver.com>
2026-05-08 14:39:32 +0900

    Fix | GCA 리뷰 반영

--- Unity/ECO/Assets/02. Scripts/02-04. Environment/Special Object/GimmickTriggerZone.cs
+++ Unity/ECO/Assets/02. Scripts/02-04. Environment/Special Object/GimmickTriggerZone.cs
@@ -11,7 +11,7 @@ public class GimmickTriggerZone : MonoBehaviour
 
     [Foldout("Project")]
     [SerializeField]
-    private bool _isOneShot = true;
+    private bool _isOneShot;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
