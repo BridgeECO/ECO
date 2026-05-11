a95499c8f061f90219a50d0373d9493b55d0164a
Mingyu Choi <mg010626@naver.com>
2026-05-11 17:50:10 +0900

    Refactor | PlayerSensor의 OverlapBiox 성능 개선

--- Unity/ECO/Assets/02. Scripts/02-02. Player/PlayerSensor.cs
+++ Unity/ECO/Assets/02. Scripts/02-02. Player/PlayerSensor.cs
@@ -25,34 +25,106 @@ public class PlayerSensor : MonoBehaviour
     private const float WALL_CHECK_DISTANCE = 0.05f;
     private const float SLIP_CHECK_BOX_SIZE = 0.1f;
 
-    public bool IsGrounded => Physics2D.OverlapBox(_feetCollider.bounds.center, _feetCollider.bounds.size, 0f, _terrainLayer) || _feetCollider.IsTouchingLayers(_platformLayer);
-    public bool IsBodyTouching => Physics2D.OverlapBox(_bodyCollider.bounds.center, _bodyCollider.bounds.size, 0f, _terrainLayer);
-    public bool IsWallTouching => Physics2D.OverlapBox(_bodyCollider.bounds.center, (Vector2)_bodyCollider.bounds.size + new Vector2(WALL_CHECK_DISTANCE * 2f, 0f), 0f, _terrainLayer);
-    public bool IsSliding => IsLeftSliding || IsRightSliding;
-    public bool IsLeftSliding
-    {
-        get
-        {
-            Vector2 checkSize = new Vector2(SLIP_CHECK_BOX_SIZE, SLIP_CHECK_BOX_SIZE);
-            Vector2 checkCenter = new Vector2(_bodyCollider.bounds.min.x, _bodyCollider.bounds.min.y);
-            return Physics2D.OverlapBox(checkCenter, checkSize, 0f, _terrainLayer | _platformLayer);
-        }
-    }
-    public bool IsRightSliding
-    {
-        get
-        {
-            Vector2 checkSize = new Vector2(SLIP_CHECK_BOX_SIZE, SLIP_CHECK_BOX_SIZE);
-            Vector2 checkCenter = new Vector2(_bodyCollider.bounds.max.x, _bodyCollider.bounds.min.y);
-            return Physics2D.OverlapBox(checkCenter, checkSize, 0f, _terrainLayer | _platformLayer);
-        }
-    }
+    public bool IsGrounded { get; private set; }
+    public bool HasPlatformEffector { get; private set; }
+    public PlatformEffector2D CurrentPlatformEffector { get; private set; }
+    public bool IsBodyTouching { get; private set; }
+    public bool IsWallTouching { get; private set; }
+    public bool IsSliding { get; private set; }
+    public bool IsLeftSliding { get; private set; }
+    public bool IsRightSliding { get; private set; }
     public float WallDirection { get; private set; }
 
+    private Collider2D[] _overlapResults = new Collider2D[1];
+    private Collider2D _lastPlatformCollider;
+    private PlatformEffector2D _cachedEffector;
+    private ContactFilter2D _contactFilter;
 
     private void Update()
     {
+        UpdateSensors();
         HandleWallDirection();
+    }
+
+    private void UpdateSensors()
+    {
+        _contactFilter.useLayerMask = true;
+        bool touchingPlatform = _feetCollider.IsTouchingLayers(_platformLayer);
+
+        UpdateGroundSensor(touchingPlatform);
+        UpdatePlatformEffectorSensor(touchingPlatform);
+        UpdateBodyAndWallSensor();
+        UpdateSlipSensor();
+    }
+
+    private void UpdateGroundSensor(bool touchingPlatform)
+    {
+        bool touchingTerrain = CheckOverlap(_feetCollider.bounds.center, _feetCollider.bounds.size, _terrainLayer);
+        IsGrounded = touchingTerrain || touchingPlatform;
+    }
+
+    private void UpdatePlatformEffectorSensor(bool touchingPlatform)
+    {
+        if (!touchingPlatform)
+        {
+            ClearPlatformCache();
+            return;
+        }
+
+        int platformCount = CheckOverlapCount(_feetCollider.bounds.center, _feetCollider.bounds.size, _platformLayer);
+        if (0 < platformCount)
+        {
+            Collider2D hit = _overlapResults[0];
+            if (!ReferenceEquals(hit, _lastPlatformCollider))
+            {
+                _lastPlatformCollider = hit;
+                _cachedEffector = hit.GetComponent<PlatformEffector2D>();
+            }
+
+            CurrentPlatformEffector = _cachedEffector;
+            HasPlatformEffector = !ReferenceEquals(_cachedEffector, null);
+        }
+        else
+        {
+            ClearPlatformCache();
+        }
+    }
+
+    private void UpdateBodyAndWallSensor()
+    {
+        IsBodyTouching = CheckOverlap(_bodyCollider.bounds.center, _bodyCollider.bounds.size, _terrainLayer);
+        IsWallTouching = CheckOverlap(_bodyCollider.bounds.center, (Vector2)_bodyCollider.bounds.size + new Vector2(WALL_CHECK_DISTANCE * 2f, 0f), _terrainLayer);
+    }
+
+    private void UpdateSlipSensor()
+    {
+        Vector2 slipCheckSize = new Vector2(SLIP_CHECK_BOX_SIZE, SLIP_CHECK_BOX_SIZE);
+        Vector2 leftCheckCenter = new Vector2(_bodyCollider.bounds.min.x, _bodyCollider.bounds.min.y);
+        Vector2 rightCheckCenter = new Vector2(_bodyCollider.bounds.max.x, _bodyCollider.bounds.min.y);
+
+        LayerMask slipLayers = _terrainLayer | _platformLayer;
+        IsLeftSliding = CheckOverlap(leftCheckCenter, slipCheckSize, slipLayers);
+        IsRightSliding = CheckOverlap(rightCheckCenter, slipCheckSize, slipLayers);
+        IsSliding = IsLeftSliding || IsRightSliding;
+    }
+
+    private bool CheckOverlap(Vector2 center, Vector2 size, LayerMask layerMask)
+    {
+        return 0 < CheckOverlapCount(center, size, layerMask);
+    }
+
+    private int CheckOverlapCount(Vector2 center, Vector2 size, LayerMask layerMask)
+    {
+        _contactFilter.layerMask = layerMask;
+        return Physics2D.OverlapBox(center, size, 0f, _contactFilter, _overlapResults);
+    }
+
+    private void ClearPlatformCache()
+    {
+        _lastPlatformCollider = null;
+        _cachedEffector = null;
+        CurrentPlatformEffector = null;
+        HasPlatformEffector = false;
     }
 
     private void HandleWallDirection()
@@ -65,10 +137,5 @@ public class PlayerSensor : MonoBehaviour
         bool isWallRight = Physics2D.Raycast
             (_bodyCollider.bounds.center, Vector2.right, _bodyCollider.bounds.extents.x + WALL_CHECK_DISTANCE, _terrainLayer);
         WallDirection = (isWallRight) ? 1f : -1f;
-    }
-
-    public Collider2D GetInteractable()
-    {
-        return Physics2D.OverlapCircle(_interactionCollider.bounds.center, _interactionCollider.radius, _interactionLayer);
     }
 }
