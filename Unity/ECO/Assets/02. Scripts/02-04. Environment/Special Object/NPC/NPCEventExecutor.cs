308a5abddec14897b281faec96ca0bf662e12ab4
Mingyu Choi <mg010626@naver.com>
2026-05-27 15:15:15 +0900

    Feat | NPCEventExecutor 구현

---
+++ Unity/ECO/Assets/02. Scripts/02-04. Environment/Special Object/NPC/NPCEventExecutor.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class NPCEventExecutor
+{
+    public void Execute(NPCEventSO eventData, PlayerInput playerInput)
+    {
+        switch (eventData.EventType)
+        {
+            case ENPCEventType.UnlockAbility:
+                ExecuteUnlockAbility(eventData.AbilityToUnlock, playerInput);
+                break;
+            case ENPCEventType.ActivateGimmick:
+                ExecuteActivateGimmick(eventData);
+                break;
+        }
+    }
+
+    private void ExecuteUnlockAbility(EPlayerUnlockableAbility abilityType, PlayerInput playerInput)
+    {
+        if (playerInput == null)
+        {
+            return;
+        }
+
+        switch (abilityType)
+        {
+            case EPlayerUnlockableAbility.Dash:
+                playerInput.IsDashLocked = false;
+                break;
+            case EPlayerUnlockableAbility.WallSlide:
+                playerInput.IsWallSlideLocked = false;
+                break;
+        }
+    }
+
+    private void ExecuteActivateGimmick(NPCEventSO eventData)
+    {
+        for (int i = 0; i < eventData.TargetTerrains.Count; i++)
+        {
+            if (eventData.TargetTerrains[i] != null)
+            {
+                eventData.TargetTerrains[i].SetEnergyActive(eventData.ActivateGimmick);
+            }
+        }
+    }
+}
