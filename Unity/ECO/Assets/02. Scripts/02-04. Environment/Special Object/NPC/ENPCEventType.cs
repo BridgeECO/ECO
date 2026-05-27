707f61588d336ad14fb168148e4d24401ba4da55
Mingyu Choi <mg010626@naver.com>
2026-05-27 15:14:56 +0900

    Add | NPCEventSO 및

---
+++ Unity/ECO/Assets/02. Scripts/02-04. Environment/Special Object/NPC/ENPCEventType.cs
@@ -0,0 +1,5 @@
+public enum ENPCEventType
+{
+    UnlockAbility,
+    ActivateGimmick
+}
---
+++ Unity/ECO/Assets/02. Scripts/02-04. Environment/Special Object/NPC/NPCEventSO.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+using VInspector;
+
+[CreateAssetMenu(fileName = "NPCEventSO", menuName = "ECO/NPC/Event Data")]
+public class NPCEventSO : ScriptableObject
+{
+    [Foldout("Base")]
+    [SerializeField]
+    private string _eventId;
+
+    [SerializeField]
+    private ENPCEventType _eventType;
+
+    [SerializeField]
+    private int _priority;
+
+    [Foldout("Dialogue")]
+    [SerializeField]
+    private string[] _dialogueLines;
+
+    [ShowIf("_eventType", ENPCEventType.UnlockAbility)]
+    [Foldout("Ability Unlock")]
+    [SerializeField]
+    private EPlayerUnlockableAbility _abilityToUnlock;
+
+    [EndIf]
+    [ShowIf("_eventType", ENPCEventType.ActivateGimmick)]
+    [Foldout("Gimmick Activation")]
+    [SerializeField]
+    private List<TerrainObject> _targetTerrains;
+
+    [SerializeField]
+    private bool _activateGimmick;
+
+    [EndIf]
+    public string EventId => _eventId;
+    public ENPCEventType EventType => _eventType;
+    public int Priority => _priority;
+    public string[] DialogueLines => _dialogueLines;
+    public EPlayerUnlockableAbility AbilityToUnlock => _abilityToUnlock;
+    public List<TerrainObject> TargetTerrains => _targetTerrains;
+    public bool ActivateGimmick => _activateGimmick;
+}
