4636a4e5e9ea9bb96e4080fa3f1aaf8472bdcc75
Mingyu Choi <mg010626@naver.com>
2026-06-19 13:40:30 +0900

    Remove | 중복된 Collider 태크 체크 로직 제거

--- Unity/ECO/Assets/02. Scripts/02-04. Environment/Special Object/Interaction/PressurePlateInteraction.cs
+++ Unity/ECO/Assets/02. Scripts/02-04. Environment/Special Object/Interaction/PressurePlateInteraction.cs
@@ -3,24 +3,16 @@ using UnityEngine;
 public class PressurePlateInteraction : InteractionBase
 {
     public PressurePlateInteraction(SpecialObjectBase target) : base(target) { }
-    
+
     public override string ValidTag => nameof(ETags.PlayerFeet);
 
     public override void OnTriggerEnter2D(Collider2D other)
     {
-        if (!other.CompareTag(nameof(ETags.PlayerFeet)))
-        {
-            return;
-        }
         TargetObject.CallSetState(true);
     }
 
     public override void OnTriggerExit2D(Collider2D other)
     {
-        if (!other.CompareTag(nameof(ETags.PlayerFeet)))
-        {
-            return;
-        }
         TargetObject.CallSetState(false);
     }
 }
