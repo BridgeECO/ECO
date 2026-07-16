faa17cc351d814812f9509f563c864c3b41469fa
Mingyu Choi <mg010626@naver.com>
2026-07-16 12:26:43 +0900

    Feat | 다중 레이어 라인 렌더러 기반 에너지 세그먼트 구현
    ----------------------
    - 기존 단일 LineRenderer 참조 구조에서 자식 오브젝트의 모든 LineRenderer를 일괄 업데이트하도록 다중 레이어 구조 지원
    - 프리팹 루트의 LineRenderer 컴포넌트 제거에 대응하여 GameObject 기준으로 인스턴스를 생성하고 소멸시키도록 변경 (메모리 누수 방지)
    - EnergySegment 내 캐싱 자료구조를 LineRenderer[] 배열에서 List<LineRenderer>로 변경

--- Unity/ECO/Assets/02. Scripts/02-04. Environment/Terrain/Energy/EnergySegment/EnergySegment.cs
+++ Unity/ECO/Assets/02. Scripts/02-04. Environment/Terrain/Energy/EnergySegment/EnergySegment.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnergySegment
@@ -6,5 +7,6 @@ public class EnergySegment
     public float TailDistance { get; set; }
     public bool IsCuttingOff { get; set; }
     public bool IsWaitingToCutOff { get; set; }
-    public LineRenderer LineRendererInstance { get; set; }
+    public GameObject GameObjectInstance { get; set; }
+    public List<LineRenderer> ChildLineRenderers { get; set; } = new List<LineRenderer>();
 }
